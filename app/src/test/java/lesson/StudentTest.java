632a0e399d05076a54419b17842f6a61a7cce234
shane panter <shane@foundationcode.com>
2022-05-20 11:00:10 -0600

    Updated tests

--- app/src/test/java/lesson/StudentTest.java
+++ app/src/test/java/lesson/StudentTest.java
@@ -5,4 +5,18 @@ import static org.testng.Assert.*;
 
 public class StudentTest {
 
+    @Test
+    public void testCollegeStudent() {
+
+        Student s = new CollegeStudent("bob");
+        assertEquals(s.name(), "bob");
+        assertFalse(s.isMinor());
+    }   
+
+    @Test
+    public void testGradeSchoolStudent(){
+        Student s = new GradeSchoolStudent("anne");
+        assertEquals(s.name(), "anne");
+        assertTrue(s.isMinor());
+    }
 }
